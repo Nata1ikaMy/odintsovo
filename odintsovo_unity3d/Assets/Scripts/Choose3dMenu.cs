9d1ff1d9fa23cada95175e24351969ec0dd2bc74
Natalya Gulyaeva <n_a_t_a_l_i_a_@mail.ru>
2017-11-09 15:33:48 +0300

    настроила срезы этажей в корпусе А

---
+++ odintsovo_unity3d/Assets/Scripts/Choose3dMenu.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Choose3dMenu : MonoBehaviour
+{
+	[System.Serializable]
+	public class Slice
+	{
+		public string 			name;
+		public string 			objName;
+		[HideInInspector]
+		public SliceController 	controller;
+	}
+
+	void Start()
+	{
+		_sceneController.SceneIsLoadEvent += CreateSliceList;
+	}
+
+	void OnDestroy()
+	{
+		_sceneController.SceneIsLoadEvent -= CreateSliceList;
+	}
+	
+	public void Show()
+	{
+		_parent.SetActive(true);
+		SetSlice(0);
+	}
+
+	public void Hide()
+	{
+		_parent.SetActive(false);
+		_slice[_currentIndex].controller.ClickUpFloor();
+		for (int i = 0; i < _slice.Length; i++)
+		{
+			_slice[i].controller.SetActiveButtons(false);
+		}
+	}
+
+	void SetSlice(int index)
+	{
+		_slice[_currentIndex].controller.ClickUpFloor();
+		_currentIndex = index;
+		_prevButton.interactable = index > 0;
+		_nextButton.interactable = _slice.Length > index + 1;
+		_text.text = _slice[_currentIndex].name;
+
+		for (int i = 0; i < _slice.Length; i++)
+		{
+			_slice[i].controller.SetActiveButtons(i == _currentIndex);
+		}
+	}
+
+	public void ClickNext()
+	{
+		if (_currentIndex + 1 < _slice.Length)
+		{
+			SetSlice(_currentIndex + 1);
+		}
+	}
+
+	public void ClickPrev()
+	{
+		if (_currentIndex - 1 >= 0)
+		{			
+			SetSlice(_currentIndex - 1);
+		}
+	}
+
+	void CreateSliceList()
+	{
+		SliceController[] controller = GameObject.FindObjectsOfType<SliceController>();
+		for (int i = 0; i < controller.Length; i++)
+		{
+			for (int j = 0; j < _slice.Length; j++)
+			{
+				if (controller[i].name == _slice[j].objName)
+				{
+					_slice[j].controller = controller[i];
+					controller[i].cameraTransform = _camera;
+				}
+			}
+		}
+	}
+
+	[SerializeField] GameObject			_parent;
+	[SerializeField] SceneController	_sceneController;
+	[SerializeField] Transform			_camera;
+	[SerializeField] Button				_prevButton;
+	[SerializeField] Button				_nextButton;
+	[SerializeField] Text				_text;
+	[SerializeField] Slice[]			_slice;
+
+	int									_currentIndex;
+}
--- odintsovo_unity3d/Assets/Scripts/SceneController.cs
+++ odintsovo_unity3d/Assets/Scripts/SceneController.cs
@@ -7,12 +7,6 @@ public class SceneController : MonoBehaviour
 {
 	public System.Action SceneIsLoadEvent;
 
-	public SliceController		sliceA { get; private set; }
-	public SliceController		sliceB { get; private set; }
-	public SliceController		sliceC { get; private set; }
-	public SliceController		sliceD { get; private set; }
-
-
 	void Start()
 	{
 		StartCoroutine(LoadYourAsyncScene());
@@ -28,37 +22,11 @@ public class SceneController : MonoBehaviour
 
 		yield return null;
 
-		SliceController[] slice = GameObject.FindObjectsOfType<SliceController>();
-		for (int i = 0; i < slice.Length; i++)
-		{
-			slice[i].cameraTransform = _camera;
-
-			if (slice[i].name == "A_Slice_Controller")
-			{
-				sliceA = slice[i];
-			}
-			else if (slice[i].name == "B_Slice_Controller")
-			{
-				sliceB = slice[i];
-			}
-			else if (slice[i].name == "C_Slice_Controller")
-			{
-				sliceC = slice[i];
-			}
-			else if (slice[i].name == "D_Slice_Controller")
-			{
-				sliceD = slice[i];
-			}
-		}
-
-		yield return null;
-
 		if (SceneIsLoadEvent != null)
 		{
 			SceneIsLoadEvent();
 		}
+
 		yield return null;
 	}
-
-	[SerializeField] Transform		_camera;
 }
