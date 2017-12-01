44236da8ac5a86f717456dd3b971d2c2f43f3a6e
Natalya Gulyaeva <n_a_t_a_l_i_a_@mail.ru>
2017-12-01 12:30:15 +0300

    копия из паблик: румсы с неправильным названием, коллизии combine вплотную к дому

---
+++ odintsovo_unity3d/Assets/ModelProject/Scripts/RoomsColor.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsColor : MonoBehaviour
+{
+	public enum ColorType
+	{
+		color1,
+		color2,
+		color3,
+		color4,
+		color5
+	}
+
+	void Start()
+	{
+        if (_colorType == ColorType.color1)
+        {
+            _color = new Color(0.8f, 0.8f, 0.8f, 1);
+        }
+        else if (_colorType == ColorType.color2)
+        {
+            _color = new Color(0.65f, 0.65f, 0.65f, 1f);
+        }
+        else if (_colorType == ColorType.color3)
+        {
+            _color = new Color(0.5f, 0.5f, 0.5f, 1f);
+        }
+        else if (_colorType == ColorType.color4)
+        {
+            _color = new Color(0.35f, 0.35f, 0.35f, 1f);
+        }
+        else if (_colorType == ColorType.color5)
+        {
+            _color = new Color(0.2f, 0.2f, 0.2f, 1f);
+        }
+
+        _colorActive = new Color(0, 0.6f, 0f, 1f);
+
+		_mesh = gameObject.GetComponent<MeshRenderer>();
+        SetDisable();
+	}
+
+    public void SetActive()
+    {
+        if (_mesh != null)
+        {
+            _mesh.material.color = _colorActive;
+        }
+    }
+
+    public void SetDisable()
+    {
+        if (_mesh != null)
+        {
+            _mesh.material.color = _color;
+        }
+    }
+
+    [SerializeField] ColorType		_colorType;
+	MeshRenderer					_mesh;
+    Color                           _color;
+    Color                           _colorActive;
+}
