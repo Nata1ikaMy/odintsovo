3a25d581b184f87ec99583e294455cccdc4a3adc
Natalya Gulyaeva <n_a_t_a_l_i_a_@mail.ru>
2017-12-06 14:47:46 +0300

    вращение текста на полу к камере

--- odintsovo_unity3d/Assets/Scripts/LookAtZ.cs
+++ odintsovo_unity3d/Assets/Scripts/LookAtZ.cs
@@ -1,29 +1,81 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class LookAtZ : MonoBehaviour {
+public class LookAtZ : MonoBehaviour
+{    
+	void Awake()
+	{
+		if (_sceneController != null)
+		{
+			_sceneController.SceneIsLoadEvent += LoadScene;
+		}
+	}
 
+	void OnDestroy()
+	{
+		if (_sceneController != null)
+		{
+			_sceneController.SceneIsLoadEvent -= LoadScene;
+		}
+	}
 
-    public Transform target;
-    [SerializeField] private Transform[] canvasTransform;
-    public bool lookAtClassic = false;
-    private Vector3 targetPostition;
-    [SerializeField] private float smooth = 2.0F;
+	void LoadScene()
+	{
+		_transformByName = new List<Transform>();
+		for (int i = 0; i < _name.Length; i++)
+		{
+			GameObject go = GameObject.Find(_name[i]);
+			if (go != null)
+			{
+				Text[] text = go.GetComponentsInChildren<Text>(true);
+				for (int j = 0; j < text.Length; j++)
+				{
+					_transformByName.Add(text[j].gameObject.transform);
+				}
+			}
+		}
+	}
 
     void Update ()
     {
-        for (int i = 0; i < canvasTransform.Length; i++)
+        for (int i = 0; i < _canvasTransform.Length; i++)
         {
-            if (canvasTransform != null)
-            {
-                if (!lookAtClassic)
-                {
-                    targetPostition = new Vector3(target.position.x, canvasTransform[i].position.y, target.position.z);
-                    canvasTransform[i].LookAt(targetPostition);
-                }
-                else
-                    canvasTransform[i].localRotation = Quaternion.Euler(-90, target.rotation.eulerAngles.y + 180, canvasTransform[i].localRotation.z);
-            }
+			SetPosition(_canvasTransform[i]);
         }
+
+		if (_transformByName != null)
+		{
+			for (int i = 0; i < _transformByName.Count; i++)
+			{
+				SetPosition(_transformByName[i]);
+			}
+		}
     }
+
+	void SetPosition(Transform obj)
+	{
+		if (_lookAtClassic)
+		{
+			obj.localEulerAngles = new Vector3(90, 180, 180 - _target.rotation.eulerAngles.y);
+		}
+		else
+		{
+			Vector3 targetPostition = new Vector3(_target.position.x, obj.position.y, _target.position.z);
+			obj.LookAt(targetPostition);
+		}
+	}
+
+	[SerializeField] Transform 			_target;
+	[SerializeField] Transform[] 		_canvasTransform;
+
+	[SerializeField] bool 				_lookAtClassic = false;
+	[SerializeField] float 				_smooth = 2.0F;
+
+	[SerializeField] SceneController	_sceneController;
+	[SerializeField] string[]			_name;
+
+	List<Transform>						_transformByName;
+
 }
