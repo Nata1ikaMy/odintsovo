a35c59e42dba4c1efe22e8fdd59edd0f1a1ad13c
Natalia Guliaeva <n_a_t_a_l_i_a_@mail.ru>
2018-02-14 14:15:37 +0300

    свет для церкви, мигание для внешнего выделения квартир

---
+++ odintsovo_unity3d/Assets/ModelProject/Scripts/AnimationAlpha.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAlpha : MonoBehaviour
+{
+	void Update()
+	{
+		if (_up)
+		{
+			_alpha += Time.deltaTime;
+		}
+		else
+		{
+			_alpha -= Time.deltaTime;
+		}
+
+		if (_alpha >= 0.8f)
+		{
+			_up = false;
+		}
+		if (_alpha <= 0.3f)
+		{
+			_up = true;
+		}
+
+		if (_mesh == null)
+		{
+			_mesh = GetComponent<MeshRenderer>();
+		}
+
+		if (_mesh != null)
+		{
+			Color color = _mesh.material.color;
+			color.a = _alpha;
+			_mesh.material.color = color;
+		}
+	}
+
+	bool _up = true;
+	float _alpha = 0.5f;
+	MeshRenderer _mesh;
+}
